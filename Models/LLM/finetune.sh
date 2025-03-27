c34c84f8adaf957de1a3dede6479cdf3c6a977ea
Djaafer Gueddou <37519960+Guedd@users.noreply.github.com>
2025-03-28 00:53:08 +0200

    Update finetune.sh

--- Models/LLM/finetune.sh
+++ Models/LLM/finetune.sh
@@ -2,11 +2,11 @@
 DATASET_DIR=PATH TO THE TEXT DATA # COMMENT OUT THIS VARIABLE IN THE FIRST FINETUNE USING THE HUGGINGFACE DATASET 
 RESULT_DIR=PATH TO SAVE MODEL, DATA, LOGS
 srun accelerate launch \
-    --config_file=accelerate_fsdp_config_xgpus.yaml \
+    --config_file=accelerater.yaml \
      --num_processes=2 \
      --num_machines=1 \
      --machine_rank=0 \
-     pllama_7b_instruct_fsdp.py \
+     pllama_cropcare_v1.py \ # FIRST PYTHON FILE WHERE WE FINETUNE THE MODEL ON THE HUGGINGFACE DATASET
      --dataset_dir ${DATASET_DIR} \
      --output_dir ${RESULT_DIR} \
      --batch_size 4\
